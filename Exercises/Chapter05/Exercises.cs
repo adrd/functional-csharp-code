d080bbe9cbe5de29caf41e81cdb7273a70dd3be2
Adrian Dinu <adrian.dinu@outlook.com>
2019-07-19 13:18:31 +0300

    Chapter05 exercises.

--- Exercises/Chapter05/Exercises.cs
+++ Exercises/Chapter05/Exercises.cs
@@ -10,7 +10,7 @@ namespace Exercises.Chapter5
 {
    public static class Exercises
    {
-      // 1. Without looking at any code or documentation (or intllisense), write the function signatures of
+      // 1. Without looking at any code or documentation (or intellisense), write the function signatures of
       // `OrderByDescending`, `Take` and `Average`, which we used to implement `AverageEarningsOfRichestQuartile`:
       static decimal AverageEarningsOfRichestQuartile(List<Person> population)
          => population
@@ -19,10 +19,25 @@ namespace Exercises.Chapter5
             .Select(p => p.Earnings)
             .Average();
 
-      // 2 Check your answer with the MSDN documentation: https://docs.microsoft.com/
-      // en-us/dotnet/api/system.linq.enumerable. How is Average different?
+        // OrderByDescending : (IEnumerable<T>, f : (T -> TKey)) -> IOrderedEnumerable<T>
+        // Take : (IEnumerable<T>, Int32) -> IEnumerable<T>
+        // Average : (IEnumerable<T>) -> T
 
-      // 3 Implement a general purpose Compose function that takes two unary functions
-      // and returns the composition of the two.
-   }
+        // 2 Check your answer with the MSDN documentation: https://docs.microsoft.com/
+        // en-us/dotnet/api/system.linq.enumerable. How is Average different?
+
+        // 3 Implement a general purpose Compose function that takes two unary functions
+        // and returns the composition of the two.
+
+        // h(x) = (f * g)(x) = f(g(x))
+
+        // f(t) : t -> t1
+        // g(t1) : t1 -> t2
+        // h(t) -> t2 = (f(g(t)) -> t2
+
+        private static Func<T, T2> Compose<T, T1, T2>(this Func<T1, T2> g, Func<T, T1> f)
+        {
+            return new Func<T, T2>(t => g(f(t)));
+        }
+    }
 }
